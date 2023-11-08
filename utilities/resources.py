d6873d8b3e76d1fa6c4b137f04a117dc92744cdf
Sevu Arjun <arjundurjun@gmail.com>
2023-11-08 14:50:41 +0530

    Final Commit

--- utilities/resources.py
+++ utilities/resources.py
@@ -2,13 +2,17 @@ import json
 
 
 class Resources:
-    url = "https://testpages.herokuapp.com/styled/tag/dynamic-table.html"
+    url = "https://testpages.herokuapp.com/styled/tag/dynamic-table.html" #source URL
 
 
 class data:
+    #reading the JSON file
     f = open("C:\\Users\\Sevu Arjun\\PycharmProjects\\cawStudios\\utilities\\data.json")
     json_file = json.load(f)
     dump = json.dumps(json_file, separators=(',', ':'))
+
+    #storing the data for assertion
+
     table101 = json_file[0]['name']
     table102 = str(json_file[0]['age'])
     table103 = json_file[0]['gender']
